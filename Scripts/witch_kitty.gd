b82029b77cb9418f3316881c2cab250d0a70f0df
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2024-07-21 14:39:23 -0700

    added diaglouge tracker and updator

--- Scripts/witch_kitty.gd
+++ Scripts/witch_kitty.gd
@@ -6,13 +6,14 @@ var new_anim = ""
 enum { KITTY_IDLE, KITTY_LICK, KITTY_LAYDOWN, KITTY_SLEEP, KITTY_ITCH, KITTY_TALK }
 
 var kitty_state = KITTY_IDLE
-
+var kitty_talkie_counter = 0
 
 @onready var interaction_area: InteractionArea = $WKInteractionArea
 
 func _ready():
 	interaction_area.interact = Callable(self, "_on_interact")
-
+	Dialogic.signal_event.connect(DialogicSignalEvent) #connects to dialogic signals
+	
 	
 	match kitty_state:
 		KITTY_IDLE:
@@ -24,6 +25,16 @@ func _ready():
 			$AnimationPlayer.play(anim)
 	pass
 
+func DialogicSignalEvent(argument:String):
+	if argument == "KittyTalkieCounter":
+		kitty_talkie_counter += 1
+	pass
+
 func _on_interact():
-	Dialogic.start("res://Dialogue/KittyExplain1.dtl")
-	await Dialogic.timeline_ended
+	if kitty_talkie_counter == 0:
+		Dialogic.start("res://Dialogue/KittyExplain1.dtl")
+		await Dialogic.timeline_ended
+	else:
+		print("I have been interacted with... but I have no dialogue to give")
+		pass
+
