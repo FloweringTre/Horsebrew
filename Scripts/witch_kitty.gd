e4cea28a4fb1bc9d4dabf41c1724700fb7772576
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2024-07-21 01:34:16 -0700

    Added interaction manager

--- Scripts/witch_kitty.gd
+++ Scripts/witch_kitty.gd
@@ -8,7 +8,12 @@ enum { KITTY_IDLE, KITTY_LICK, KITTY_LAYDOWN, KITTY_SLEEP, KITTY_ITCH, KITTY_TAL
 var kitty_state = KITTY_IDLE
 
 
+@onready var interaction_area: InteractionArea = $WKInteractionArea
+
 func _ready():
+	interaction_area.interact = Callable(self, "_on_interact")
+
+	
 	match kitty_state:
 		KITTY_IDLE:
 			new_anim = "idle"
@@ -18,3 +23,7 @@ func _ready():
 			anim = new_anim
 			$AnimationPlayer.play(anim)
 	pass
+
+func _on_interact():
+	Dialogic.start("res://Dialogue/KittyExplain1.dtl")
+	await Dialogic.timeline_ended
