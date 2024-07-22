6139b205b503941cfb72ea94961c00e0cf41ca40
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2024-07-21 21:12:08 -0700

    added dynamic potion pot

---
+++ GameParts/Items/potion_pot.gd
@@ -0,0 +1,20 @@
+extends Node2D
+
+
+func _ready():
+	$AnimationPlayer.play("RESET")
+	Dialogic.signal_event.connect(DialogicSignalEvent) #connects to dialogic signals
+
+func DialogicSignalEvent(argument:String):
+	if argument == "DaffodilReady":
+		$AnimationPlayer.play("sizzling_lotus")
+	if argument == "ShellReady":
+		$AnimationPlayer.play("sizzling_daffodil")
+	if argument == "BerryReady":
+		$AnimationPlayer.play("bubbling_berry")
+	if argument == "StarReady":
+		$AnimationPlayer.play("bubbling_berry")
+	if argument == "PotionComplete":
+		$AnimationPlayer.play("bubbling_finished")
+	pass
+
