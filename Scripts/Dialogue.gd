798eab67d526fd6b8b07a9fc221dae29376fda55
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2024-07-28 11:58:43 -0700

    Forced to inital dialogue to run first

--- Scripts/Dialogue.gd
+++ Scripts/Dialogue.gd
@@ -1,5 +1,14 @@
 extends Node2D
 
+@onready var GameStart = false
+
 func _ready():
+	Dialogic.signal_event.connect(DialogicSignalEvent) #connects to dialogic signals
 	Dialogic.start("res://Dialogue/InitialDialogue.dtl")
 	pass
+	
+func DialogicSignalEvent(argument:String):
+	if argument == "InitalDialogueEnd":
+		GameStart = true
+	pass
+
