7d5a7cbba2084eb70e06f6ba306f6489aed124a3
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2024-07-29 23:03:50 -0700

    updated for main menu animations

--- GameParts/Items/potion_pot.gd
+++ GameParts/Items/potion_pot.gd
@@ -1,16 +1,18 @@
 extends Node2D
 
 func _ready():
-	$AnimationPlayer.play("RESET")
+	$AnimationPlayer.play("bubbling_finished")
 	Dialogic.signal_event.connect(DialogicSignalEvent) #connects to dialogic signals
 
 func DialogicSignalEvent(argument:String):
+	if argument == "InitalDialogueEnd":
+		$AnimationPlayer.play("RESET")
 	if argument == "DaffodilReady":
 		$AnimationPlayer.play("sizzling_lotus")
 	if argument == "ShellReady":
 		$AnimationPlayer.play("sizzling_daffodil")
 	if argument == "BerryReady":
-		$AnimationPlayer.play("bubbling_berry")
+		$AnimationPlayer.play("bubbling_shell")
 	if argument == "StarReady":
 		$AnimationPlayer.play("bubbling_berry")
 	if argument == "PotionComplete":
