5ffa5f3ef512b8f06fdd336946df3206bf5b62d8
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2024-07-30 23:28:01 -0700

    Added global level checker for checking across scenes

--- GameParts/SceneSwitching/Globals.gd
+++ GameParts/SceneSwitching/Globals.gd
@@ -3,7 +3,16 @@ extends Node
 # warning-ignore:unused_class_variable
 var spawnpoint = ""
 var current_level = ""
+var horse_level = 0
 
 func _ready():
 	RenderingServer.set_default_clear_color(Color.WHITE)
+	Dialogic.signal_event.connect(DialogicSignalEvent)
 	
+func DialogicSignalEvent(argument:String):
+	if argument == "LevelUp":
+		level_up()
+	pass
+
+func level_up():
+	horse_level += 1
