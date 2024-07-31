b180410308f2d1a070b4ce0214effb5e5691af75
kyraltre <123285225+FloweringTre@users.noreply.github.com>
2024-07-30 23:22:01 -0700

    Deleted unneeded files from build

--- Assets/EPIC RPG World Pack - Grass Land V. 1.5.1/TiledMap Editor/DESERT_TREES-demo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="DESERT_TREES-demo" tilewidth="32" tileheight="32" tilecount="20" columns="4">
- <image source="../../DESERT_TREES-demo.png" width="128" height="176"/>
-</tileset>
